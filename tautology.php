e9a1b96f33127eb3070f55e2d5b70a4f53b52d5c
janani14iyer@gmail.com <janani14iyer@gmail.com>
2016-09-11 21:46:27 +0530

    first commit

---
+++ tautology.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Enter the Equation: " . PHP_EOL;
+
+// the script will wait here until the user has entered something and hit ENTER
+$Equation = read_stdin();
+
+echo "The equation you entered is $Equation" . PHP_EOL;
+
+// function to read from the command line
+function read_stdin()
+{
+        $fr=fopen("php://stdin","r");   
+        $input = fgets($fr,128);        
+        $input = rtrim($input);         
+        fclose ($fr);                   
+        return $input;                  
+}
+
+?>
