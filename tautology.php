25e13849f252d71bbe71c77ab29b504d76ac7baa
janani14iyer@gmail.com <janani14iyer@gmail.com>
2016-09-13 01:59:55 +0530

    expression function

--- tautology.php
+++ tautology.php
@@ -4,40 +4,28 @@ echo "Enter the Equation: " . PHP_EOL;
 
 // the script will wait here until the user has entered something and hit ENTER
 $eqtn = read_stdin();
+$eqtn = str_replace(' ', '', $eqtn);
 
 echo "The equation you entered is $eqtn" . PHP_EOL;
 
 // looking at each charater in the equation
 $len = strlen($eqtn);
 echo "length of equation is $len" . PHP_EOL;
-$char = array();
+//$char = array();
 
 for ($i=0; $i<$len; $i++)
 {
-	if ($eqtn[$i]==" ")
-	{
-		//do nothing --> removes whitespace in between the equation
-	}
-	else if ($eqtn[$i]=="!")
-	{
-		// call the Not function
-		//$char[$i] = func_not($eqtn[$i+1]);
-		$char[$i] = $eqtn[$i+1];
-	}
-	elseif ($eqtn[$i]=="&") {
-		// call the and function
-		$char[$i] = func_and($eqtn[$i-1], $eqtn[$i+1]);
-	}
-	elseif ($eqtn[$i]=="|") {
-		// call the OR function
-		$char[$i] = func_or($eqtn[$i-1], $eqtn[$i+1]);
-	}
-	else
-	{
-		$char[$i] = $eqtn[$i];	
-		echo "$char[$i]";
-	}
-	
+	if ($eqtn[$i]=="&" || $eqtn[$i]=="|")
+ 	{
+ 		//call the expression function
+ 		$char[$i] = func_exprsn($eqtn[$i-1], $eqtn[$i], $eqtn[$i+1]);
+ 		echo "$char[$i]";
+ 	}
+ 	else
+ 	{
+ 		$char[$i] = $eqtn[$i];	
+ 		//echo "$char[$i]";
+ 	}
 }
 
 // function to read from the command line
@@ -50,22 +38,30 @@ function read_stdin()
         return $input;                  
 }
 
-// function for NOT operation
-function func_not($op)
+function func_exprsn($op1, $op2, $op3)
 {
-	return $op;
-}
+	if ($op1==$op3)
+	{
+		if ($op1=="T")
+			return 'T';	
+		else if ($op1=="F")
+			return 'F';
+		else
+			return $op1;
+	}
 
-// function for AND operation
-function func_and($op1, $op2)
-{
-	return $op1 && $op2;	
-}
-
-// function for OR operation
-function func_or($op1, $op2)
-{
-	return $op1 || $op2;	
+	else if ($op1!=$op3)
+	{
+		if (($op1=="T" && $op3=="F") || ($op1=="F" && $op3=="T"))
+		{
+			if ($op2 =="&")
+				return 'F';
+			elseif ($op2=='|') 
+				return 'T';
+		}
+		else
+			return $op1.$op2.$op3;
+	}
 }
 
 ?>
